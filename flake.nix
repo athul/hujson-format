3b91a5096ccdf4c4f351a625139b55030cc83c70
Athul Cyriac Ajay <athul8720@gmail.com>
2024-04-25 01:57:20 +0530

    Nixxxxx

---
+++ flake.nix
@@ -0,0 +1,53 @@
+{
+  description = "Husjon Validator - gomod2nix flake";
+
+  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+  inputs.flake-utils.url = "github:numtide/flake-utils";
+  inputs.gomod2nix.url = "github:nix-community/gomod2nix";
+  inputs.gomod2nix.inputs.nixpkgs.follows = "nixpkgs";
+  inputs.gomod2nix.inputs.flake-utils.follows = "flake-utils";
+
+  outputs = { self, nixpkgs, flake-utils, gomod2nix }:
+    (flake-utils.lib.eachDefaultSystem
+      (system:
+        let
+          pkgs = import nixpkgs {
+            inherit system;
+            overlays = [ gomod2nix.overlays.default ];
+          };
+
+          # The current default sdk for macOS fails to compile go projects, so we use a newer one for now.
+          # This has no effect on other platforms.
+          callPackage = pkgs.darwin.apple_sdk_11_0.callPackage or pkgs.callPackage;
+        in
+        rec {
+          hjsonapp = callPackage ./. {
+            inherit (gomod2nix.legacyPackages.${system}) buildhjsonapplication;
+          };
+          packages.dockerContainer = pkgs.dockerTools.buildImage {
+            name = "hujson-validator";
+            tag = "latest";
+            created = "now";
+            copyToRoot = pkgs.buildEnv {
+              name = "base-dir";
+              paths = [
+                hjsonapp
+              ];
+              pathsToLink = [ "/bin" ];
+            };
+            config = {
+              Cmd = [ "${hjsonapp}/bin/hujson-validator" ];
+              ExposedPorts = {
+                "8080/tcp" = { };
+              };
+            };
+          };
+          packages.default = hjsonapp;
+          devShells.default = callPackage
+            ./shell.nix
+            {
+              inherit (gomod2nix.legacyPackages.${system}) mkGoEnv gomod2nix;
+            };
+        })
+    );
+}
--- go.mod
+++ go.mod
@@ -2,4 +2,4 @@ module github.com/athul/hujson-validator
 
 go 1.19
 
-require github.com/tailscale/hujson v0.0.0-20221223112325-20486734a56a // indirect
+require github.com/tailscale/hujson v0.0.0-20221223112325-20486734a56a
